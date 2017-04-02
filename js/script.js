8e192798bde32516955786a0200a9afbcf6cd6ac
charanrain <cvcharanrj@gmail.com>
2017-04-02 22:05:36 +0530

    added my github name

--- js/script.js
+++ js/script.js
@@ -3,6 +3,12 @@ var modal = document.getElementById("modal-wrap");
 
   	modal.style.visibility = (modal.style.visibility == "visible") ? "hidden" : "visible";
   }
+  document.addEventListener('keyup', function(e) {
+    if (e.keyCode == 27) {
+        overlay();
+    }
+});
+
 function hideOverlay(){
 modal.style.visibility = (modal.style.visibility == "hidden") ? "visible" : "hidden";
 }
