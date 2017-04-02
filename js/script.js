85a30e727c33b94bbbb9c9c36bc5abb9a57915e7
charanrain <cvcharanrj@gmail.com>
2017-04-02 11:42:30 +0530

    First commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var modal = document.getElementById("modal-wrap");
+  function overlay() {
+
+  	modal.style.visibility = (modal.style.visibility == "visible") ? "hidden" : "visible";
+  }
+function hideOverlay(){
+modal.style.visibility = (modal.style.visibility == "hidden") ? "visible" : "hidden";
+}
